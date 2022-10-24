84c6bf06134637f8bf430430430fc6d94b48eda4
Just-gemer <115401329+Just-gemer@users.noreply.github.com>
2022-10-24 10:35:59 +0500

    Add files via upload

---
+++ jdu-skins-adder.py
@@ -0,0 +1,24 @@
+#JDU SKINS ADDER BY Just gemer
+import os
+print("JDU SKINS ADDER BY Just gemer")
+jdver = str(input("jdVersion: "))
+price = str(input("mojoPrice(type 0 if you dont want it): "))
+unlock = str(input("unlockType: "))
+url = str(input("url: "))
+num = str(input("skin number: "))
+
+# Skins Adder (WORKS ONLY WITH EXISTING JSON)
+skin = open("skins.json", "w", encoding="utf-8")
+skin.write('''{
+    "''' + num + '''": {
+        "__class": "OnlineCustomizableItem",
+        "jdVersion": ''' + jdver + ''',
+        "mojoPrice": ''' + price + ''',
+        "status": 3,
+        "unlockType": ''' + unlock + ''',
+        "url": "''' + url + '''",
+        "itemType": 1,
+        "visibility": 1
+    }
+}''')
+skin.close()
